1a4f3f2ca91cc1e8a3ff909944bb6bfe9b03e248
Ushitora Anqou <ushitora@anqou.net>
2022-12-01 16:00:36 +0900

    Better usage and help

--- bin/main.ml
+++ bin/main.ml
@@ -1,7 +1,7 @@
 open Sympatch
 
 let parse_command_line () =
-  let usage_msg = "sympatch -pNUM SRC-DIR DST-DIR < PATCH-FILE" in
+  let usage_msg = "sympatch -p NUM SRC-DIR DST-DIR < PATCH-FILE" in
   let p_num = ref 0 in
   let opt = ref false in
   let input_files = ref [] in
@@ -13,7 +13,8 @@ let parse_command_line () =
   in
   let anon_fun filename = input_files := filename :: !input_files in
   Arg.parse speclist anon_fun usage_msg;
-  if List.length !input_files <> 2 then failwith usage_msg
+  if List.length !input_files <> 2 then
+    failwith "Invalid command-line arguments. Check usage by `-help`."
   else
     let files = List.rev !input_files in
     let src_dir = List.nth files 0 in
@@ -22,8 +23,12 @@ let parse_command_line () =
     (!p_num, !opt, src_dir, dst_dir, patch)
 
 let () =
-  let p_num, opt, src_dir, dst_dir, patch = parse_command_line () in
-  let patch = Patch.of_string patch in
-  src_dir |> Fs.of_path
-  |> Patch.patch ~opt patch p_num
-  |> Fs.instantiate dst_dir
+  try
+    let p_num, opt, src_dir, dst_dir, patch = parse_command_line () in
+    let patch = Patch.of_string patch in
+    src_dir |> Fs.of_path
+    |> Patch.patch ~opt patch p_num
+    |> Fs.instantiate dst_dir
+  with Failure s ->
+    Printf.fprintf stderr "%s\n" s;
+    exit 1
